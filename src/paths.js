67b5755a3ee0e524999d3d04c7cc025cb3609638
Artem Korolev <artkor@live.ru>
2018-10-18 19:36:31 +0300

    fix(paths): fixing default paths of entry files

--- src/paths.js
+++ src/paths.js
@@ -3,8 +3,8 @@ const path = require("path");
 module.exports = {
 	root: path.resolve("./"),
 	outputPath: path.resolve("build"),
-	entryPath: path.resolve(process.env.ENTRY_FILE || "index.jsx"),
-	templatePath: path.resolve("index.html"),
+	entryPath: path.resolve(process.env.ENTRY_FILE || "src/App.jsx"),
+	templatePath: path.resolve(process.env.TEMPLATE_FILE || "src/index.html"),
 	imagesFolder: "assets/images",
 	fontsFolder: "assets/fonts",
 	cssFolder: "assets/css",
