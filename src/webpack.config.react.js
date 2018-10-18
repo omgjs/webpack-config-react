84c4dcb57c76116c385adff69fd583dc3345bb31
Artem Korolev <artkor@live.ru>
2018-10-18 21:05:27 +0300

    fix(log): little bit of output to console

--- src/webpack.config.react.js
+++ src/webpack.config.react.js
@@ -5,7 +5,8 @@ const envs = {
 	development: "dev",
 	production: "prod",
 };
-/* eslint-disable global-require,import/no-dynamic-require */
+/* eslint-disable global-require,import/no-dynamic-require,no-console */
 const env = envs[process.env.NODE_ENV || "development"];
+console.log(`Webpack config is using ${process.env.NODE_ENV} environment`);
 const envConfig = require(`./webpack.config.${env}`);
 module.exports = webpackMerge(common, envConfig);
